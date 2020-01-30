30ce8efd304cde7b50aa98970bf967b17e636a71
Kacper Olek <kacper.olek@cognifide.com>
2020-01-30 14:31:34 +0100

    RND-767 | Expanding on click

--- src/js/components/fragment.jsx
+++ src/js/components/fragment.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 
 const Fragment = (props) => {
   const { success, id, type } = props;
+  const [expanded, setExpanded] = useState(false);
+  const fragment = useRef(null);
+  useEffect(() => {
+    if (expanded) {
+      fragment.current.classList.add('expanded');
+    } else {
+      fragment.current.classList.remove('expanded');
+    }
+  }, [expanded]);
   const statusClass = success ? 'fragment-status-success' : 'fragment-status-failure';
   function handleClick(event) {
-    console.log(event.currentTarget);
+    event.preventDefault();
+    setExpanded(!expanded);
+  }
+  function handleEnter(event) {
+    if (event.keyCode === 13) {
+      setExpanded(!expanded);
+    }
   }
   return (
-    <div role="menuitem" className="fragment-item" tabIndex="0" onClick={handleClick} onKeyDown={handleClick}>
+    <div ref={fragment} role="menuitem" className="fragment-item" tabIndex="0" onClick={handleClick} onKeyDown={handleEnter}>
       <div className="fragment-status-wrapper">
         <div className={`fragment-status ${statusClass}`} />
       </div>
