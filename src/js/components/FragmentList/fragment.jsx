1daff6d86ca264a816e11e810c82c7e49d7ba8a9
Kacper Olek <kacper.olek@cognifide.com>
2020-02-10 10:05:14 +0100

    RND-767 | Removed useRef import

--- src/js/components/FragmentList/fragment.jsx
+++ src/js/components/FragmentList/fragment.jsx
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import NodeList from './nodeList';
