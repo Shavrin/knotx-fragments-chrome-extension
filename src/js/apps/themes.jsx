c1843d4944d8cb5457e4e4ca442bfa98127b42b8
Kacper Olek <kacper.olek@cognifide.com>
2020-02-11 15:03:28 +0100

    RND-767 | Separated themes to another file

---
+++ src/js/apps/themes.jsx
@@ -0,0 +1,19 @@
+export const defaultTheme = {
+  textColor: '#2c2c2c',
+  buttonBgColor: '#c1c1c1',
+  fragmentHighlight: '#0056d827',
+  borderColor: '#adadad56',
+  oddFragmentBgColor: '#dcdcdc44',
+  nodeHighlight: '#0e000034',
+  oddNodeBgColor: '#54545414',
+};
+
+export const darkTheme = {
+  textColor: '#d3d3d3',
+  buttonBgColor: '#6e6e6e',
+  fragmentHighlight: '#0026ff34',
+  borderColor: '#dcdcdc44',
+  oddFragmentBgColor: '#ffffff13',
+  nodeHighlight: '#8fe3ff42',
+  oddNodeBgColor: '#add8e60e',
+};
