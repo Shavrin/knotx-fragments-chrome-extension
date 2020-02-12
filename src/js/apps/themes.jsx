567af0a5eb5d8670e6538cdc3a341d6c08de8798
Kacper Olek <kacper.olek@cognifide.com>
2020-02-12 15:20:10 +0100

    RND-767 | Exported color codes to constants

--- src/js/apps/themes.jsx
+++ src/js/apps/themes.jsx
@@ -1,19 +1,21 @@
+import { COLORS } from '../helpers/constants';
+
 export const defaultTheme = {
-  textColor: '#2c2c2c',
-  buttonBgColor: '#c1c1c1',
-  fragmentHighlight: '#0056d827',
-  borderColor: '#adadad56',
-  oddFragmentBgColor: '#dcdcdc44',
-  nodeHighlight: '#0e000034',
-  oddNodeBgColor: '#54545414',
+  textColor: COLORS.TEXT_COLOR,
+  buttonBgColor: COLORS.BUTTON_BG_COLOR,
+  fragmentHighlight: COLORS.FRAGMENT_HIGHLIGHT,
+  borderColor: COLORS.BORDER_COLOR,
+  oddFragmentBgColor: COLORS.ODD_FRAGMENT_BG_COLOR,
+  nodeHighlight: COLORS.NODE_HIGHLIGHT,
+  oddNodeBgColor: COLORS.ODD_NODE_BG_COLOR,
 };
 
 export const darkTheme = {
-  textColor: '#d3d3d3',
-  buttonBgColor: '#6e6e6e',
-  fragmentHighlight: '#0026ff34',
-  borderColor: '#dcdcdc44',
-  oddFragmentBgColor: '#ffffff13',
-  nodeHighlight: '#8fe3ff42',
-  oddNodeBgColor: '#add8e60e',
+  textColor: COLORS.DARK.TEXT_COLOR,
+  buttonBgColor: COLORS.DARK.BUTTON_BG_COLOR,
+  fragmentHighlight: COLORS.DARK.FRAGMENT_HIGHLIGHT,
+  borderColor: COLORS.DARK.BORDER_COLOR,
+  oddFragmentBgColor: COLORS.DARK.ODD_FRAGMENT_BG_COLOR,
+  nodeHighlight: COLORS.DARK.NODE_HIGHLIGHT,
+  oddNodeBgColor: COLORS.DARK.ODD_NODE_BG_COLOR,
 };
